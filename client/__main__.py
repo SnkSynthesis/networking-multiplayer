7fb3c48acf4b3485c1660f90880cb590f866c9bb
IsaiahT-Tech <63564966+IsaiahT-Tech@users.noreply.github.com>
2021-04-05 09:29:22 -0700

    Modifed repo

--- client/__main__.py
+++ client/__main__.py
@@ -1,26 +1,4 @@
 import pygame 
-<<<<<<< HEAD
-
-def setup():
-    pygame.init()
-    display = pygame.display.set_mode((1000, 1000))
-
-class Object:
-
-    def __init__(self, x, y, width, height):
-        self.x = x 
-        self.y = y 
-        self.width = width 
-        self.height = height 
-
-cube = Object()
-
-def main():
-    setup()
-
-if __name__ == '__main__':
-    main()
-=======
 import typing
 import time
 
@@ -29,17 +7,17 @@ d_y = 600
 white = (255, 255, 255)
 
 
-def setup():
-    pygame.init()
+
+pygame.init()
 
 display = pygame.display.set_mode((d_x, d_y))
 pygame.display.set_caption("Multiplayer Cubes")
 
 class Shape:
 
-    def __init__(self, posx, posy):
-        self.posx = posx
-        self.posy = posy
+    # def __init__(self, posx, posy):
+    #     self.posx = posx
+    #     self.posy = posy
 
 
     def draw(self, display, color, posx, posy, length, width):
@@ -47,55 +25,60 @@ class Shape:
         self.width = width
         self.display = display
         self.color = color
-        # self.posx = posx
-        # self.posy = posy
+        self.posx = posx
+        self.posy = posy
+
 
         pygame.draw.rect(self.display, self.color, [self.posx, self.posy, self.length, self.width])
+        
     
-    def move(self, x, y):
-        self.x = x   
-        self.y = y 
+    def move(self, distance):
+        self.distance = distance
         key = pygame.key.get_pressed()
         
         if key[pygame.K_LEFT]:
-            
-            self.x -= distance
+
+            self.posx -= distance
         elif key[pygame.K_RIGHT]:
             
-            x += distance 
+            self.posx += distance 
         
         elif key[pygame.K_UP]:
 
-            y -= distance 
+            self.posy -= distance 
 
         elif key[pygame.K_DOWN]:
 
-            y += distance
+            self.posy += distance
         
             
     
-square = Shape(0, 0)
-square.draw(display, white, 0, 0, 100, 100)
-
-square.move(0, 0)
-
-pygame.display.flip()
+square = Shape()
 
 
 
-def run():
-    run = True
-    while run == True: 
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                run = False
-                pygame.quit()
 
+
+
+
+run = True
+while run: 
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            run = False
+            pygame.quit()
+    
+    square.draw(display, white, 0, 0, 100, 100)
+    square.move(500)
+
+
+    pygame.display.flip()
+        
+    
 
 def main():
     setup()
-    run()
+    
 
 if __name__ == '__main__':
     main() 
->>>>>>> 92b0267a149dcfdce9e93aec0c8288a6f8ec7b89
